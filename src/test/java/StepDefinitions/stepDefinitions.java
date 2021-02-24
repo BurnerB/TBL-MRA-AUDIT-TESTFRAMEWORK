84579e9b119fc29bb0f6e2060eeed306b4501218
Barnaby Kamau <45785786+BurnerB@users.noreply.github.com>
2021-02-24 13:29:22 +0300

    Login to crm as AuditManager and pick

--- src/test/java/StepDefinitions/stepDefinitions.java
+++ src/test/java/StepDefinitions/stepDefinitions.java
@@ -92,6 +92,69 @@ public class stepDefinitions extends BaseClass {
         driver.findElement(By.id("Logout")).click();
     }
 
+    //---------------------------------------------------------------------Verify the Process of Assign Audit Case-----------------------------------------------------------------------------------------------//
+    @Given("^Open CRM URL Module as \"([^\"]*)\"$")
+    public void open_crm_url_module_as_something(String strArg1) throws Throwable {
+            driver = BaseClass.getDriver();
+            driver.get("https://"+strArg1+":Passw0rd@trips-crm.mra.mw:5555/TripsWorkflow/main.aspx");
+        }
+
+    @And("^Close Popup Window$")
+    public void close_Popup_Window() throws Throwable {
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        WebElement specificframe = (driver.findElement(By.id(Pro.getProperty("CRM_ExploreCrmWindow_Frame__ID"))));
+        driver.switchTo().frame(specificframe);
+        WebDriverWait CloseWindow = new WebDriverWait(driver, 60);
+        CloseWindow.until(ExpectedConditions.elementToBeClickable(By.id(Pro.getProperty("CRM_ExploreCrmWindow_Frame_Close_ID")))).click();
+    }
+    @And("^Click on Case management dropdown$")
+    public void click_on_case_management_dropdown() throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"TabCS\"]/a/span")).click();
+    }
+
+    @And("^click on Queues$")
+    public void click_on_revenue_collection_application() throws Throwable {
+        driver.findElement(By.xpath("//*[text()='Queues']")).click();
+    }
+
+
+    @Then("^switch to frame0$")
+    public void switch_to_frame0() throws Throwable {
+        driver.switchTo().defaultContent();
+        WebDriverWait wait=new WebDriverWait(driver, 30);
+        WebElement specificframe = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(Pro.getProperty("NextStage_Frame_ID"))));
+        driver.switchTo().frame(specificframe);
+        Thread.sleep(3000);
+
+    }
+
+    @And("^enters Audit reference number in search results$")
+    public void enters_exemption_reference_number_in_search_results() throws Throwable {
+        WebDriverWait wait=new WebDriverWait(driver, 20);
+        WebElement search = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("crmGrid_findCriteria")));
+//        search.sendKeys(sharedatastep.E_CRMARN);
+    	search.sendKeys("*AV/000000020/2020");
+        Thread.sleep(2000);
+        search.sendKeys(Keys.ENTER);
+
+        Thread.sleep(2000);
+    }
+
+    @And("^picks the audit case$")
+    public void picks_the_audit_case() throws Throwable {
+        WebElement pickCheckBox = driver.findElement(By.xpath("//input[@type='checkbox']"));
+
+        Actions actions = new Actions(driver);
+        actions.doubleClick(pickCheckBox).perform();
+
+        driver.switchTo().defaultContent();
+        Thread.sleep(9000);
+    }
+
+
+
+
 
 
 }
---
+++ src/test/resources/FeatureFiles/AssignAuditCase_SUC-21-08.feature
@@ -0,0 +1,13 @@
+Feature: [SUC:21-08] Assign an Audit Case
+
+
+Scenario: UAT_M8-21-08-01-UAT_M8-21-08-02-Verify the Process of Assign Audit Case
+ Given Open CRM URL Module as "AuditManager1"
+ And Close Popup Window
+ And Click on Case management dropdown
+ And click on Queues
+ Then switch to frame0
+ And enters Audit reference number in search results
+And picks the audit case
+
+
